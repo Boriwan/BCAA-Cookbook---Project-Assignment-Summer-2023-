1cb2d0f844dbce0a6af635031bbe00fe5ca30c7b
Boriwan <boris.bocek@educanet.com>
2023-03-08 19:29:36 +0100

    progress

--- server/controllers/recipe.js
+++ server/controllers/recipe.js
@@ -1,17 +1,24 @@
 var express = require("express");
 var router = express.Router();
+const CreateAbl = require("../create-abl");
+
 
 router.get("/getRecipe", function (req, res) {
   res.send("GET Recipe");
+});
+
+router.post("/postRecipe", function (req, res) {
+  CreateAbl(req, res);
 });
 
 router.put("/putRecipe", (req, res) => {
   res.send("Got a PUT request at /putRecipe");
 });
 
-router.post("/postRecipe", function (req, res) {
-  res.send("POST recipe");
-});
+// router.delete("/deleteRecipe", (req, res) => {
+
+// }
+
 
 //export this router to use in our index.js
 module.exports = router;
--- server/dao/recipe-dao.js
+++ server/dao/recipe-dao.js
@@ -1,1 +1,47 @@
 //implement dao
+"use-strict";
+const fs = require("fs");
+const path = require("path");
+const crypto = require("crypto");
+
+const DEFAULT_STORAGE_PATH = path.join(
+  __dirname,
+  "server",
+  "storage",
+  "recipes.json"
+);
+
+class RecipeDao {
+  constructor(storagePath) {
+    this.recipeStoragePath = storagePath ? storagePath : DEFAULT_STORAGE_PATH;
+  }
+
+  create(object) {
+    let recipeList = this._listAll();
+    object.id = crypto.randomBytes(8).toString("hex");
+    recipeList.push(object);
+    fs.writeFileSync(this._getStoragePath(), JSON.stringify(recipeList));
+    return object;
+  }
+
+  list() {
+    return this._listAll();
+  }
+
+  _listAll() {
+    let listAll;
+    try {
+      const fileData = fs.readFileSync(this._getStoragePath());
+      if (fileData) listAll = JSON.parse(fileData);
+    } catch (e) {
+      listAll = [];
+    }
+    return listAll;
+  }
+
+  _getStoragePath() {
+    return this.recipeStoragePath;
+  }
+}
+
+module.exports = RecipeDao;
