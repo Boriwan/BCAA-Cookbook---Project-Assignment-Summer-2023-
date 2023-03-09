6821aae7824f6fc640b085361e77477d002cdb4d
Boriwan <boris.bocek@educanet.com>
2023-03-09 12:42:09 +0100

    input requirements for creating a recipe

--- server/abl/recipe-abl/create-abl.js
+++ server/abl/recipe-abl/create-abl.js
@@ -5,8 +5,31 @@ const path = require("path");
 let dao = new RecipeDao(path.join(__dirname, "..", "..", "storage", "recipes.json"));
 
 function CreateAbl(req, res) {
-  let recipe = req.body;
-  recipe = dao.create(recipe);
+  let body = req.body;
+  if (!body.name || !body.desc || !body.finalAmount || !body.prepLength || !body.ingredients) {
+    return res.status(400).json({error: 'Invalid input: code parameter is missing.'});
+  }
+
+  let recipe = {
+    name: body.name,
+    desc: body.desc,
+    finalAmount: body.finalAmount,
+    prepLength: body.prepLength,
+    ingredients: body.ingredients
+  };
+
+  try {
+      recipe = dao.create(recipe);
+  } catch (e) {
+    // if (e.id === "DUPLICATE_CODE") {
+    //   res.status(400);
+    // } else {
+    //   res.status(500);
+    // }
+     res.status(500);
+    return res.json({error: e.message});
+  }
+
   res.send(recipe);
 }
 
