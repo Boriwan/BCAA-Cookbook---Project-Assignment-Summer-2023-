60b4dadbb08b57668476d1ef555c2d5477226e01
Jakub Stadnik <106155603+jakubstadnik03@users.noreply.github.com>
2023-03-15 19:28:56 +0100

    update recipe, delete done

--- server/abl/recipe-abl/create-abl.js
+++ server/abl/recipe-abl/create-abl.js
@@ -2,12 +2,22 @@
 const RecipeDao = require("../../dao/recipe-dao");
 const path = require("path");
 
-let dao = new RecipeDao(path.join(__dirname, "..", "..", "storage", "recipes.json"));
+let dao = new RecipeDao(
+  path.join(__dirname, "..", "..", "storage", "recipes.json")
+);
 
 function CreateAbl(req, res) {
   let body = req.body;
-  if (!body.name || !body.desc || !body.finalAmount || !body.prepLength || !body.ingredients) {
-    return res.status(400).json({error: 'Invalid input: code parameter is missing.'});
+  if (
+    !body.name ||
+    !body.desc ||
+    !body.finalAmount ||
+    !body.prepLength ||
+    !body.ingredients
+  ) {
+    return res
+      .status(400)
+      .json({ error: "Invalid input: code parameter is missing." });
   }
 
   let recipe = {
@@ -15,19 +25,20 @@ function CreateAbl(req, res) {
     desc: body.desc,
     finalAmount: body.finalAmount,
     prepLength: body.prepLength,
-    ingredients: body.ingredients
+    categories: body.categories,
+    ingredients: body.ingredients,
   };
 
   try {
-      recipe = dao.create(recipe);
+    recipe = dao.create(recipe);
   } catch (e) {
     // if (e.id === "DUPLICATE_CODE") {
     //   res.status(400);
     // } else {
     //   res.status(500);
     // }
-     res.status(500);
-    return res.json({error: e.message});
+    res.status(500);
+    return res.json({ error: e.message });
   }
 
   res.send(recipe);
--- server/abl/recipe-abl/update-abl.js
+++ server/abl/recipe-abl/update-abl.js
@@ -1,19 +1,21 @@
+// TODO
+
 const RecipeDao = require("../../dao/recipe-dao");
 const path = require("path");
-
 let dao = new RecipeDao(
   path.join(__dirname, "..", "..", "storage", "recipes.json")
 );
 
-// console.log(recipeList);
-
-function GetAbl(req, res) {
+function UpdateAbl(req, res) {
   const recipe = dao.get(req.params.id);
+  const updatedObject = { ...recipe, ...req.params };
+
   if (recipe) {
-    res.json(recipe);
+    dao.update(updatedObject);
   } else {
     res.status(400).json({ error: "Recipe does not exist" });
   }
-}
 
-module.exports = GetAbl;
+  res.json(updatedObject);
+}
+module.exports = UpdateAbl;
